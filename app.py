1f77a1feee18903a4b2d71876ad827d15ebe3d8b
Pratik Biswas <pratik.biswas@exavalu.com>
2025-08-18 20:21:53 +0530

    updated code

--- app.py
+++ app.py
@@ -4,6 +4,6 @@ def lambda_handler(event, context):
     return {
         "statusCode": 200,
         "body": json.dumps({
-            "message": "hello world",
+            "message": "hello world updated",
         }),
     }
